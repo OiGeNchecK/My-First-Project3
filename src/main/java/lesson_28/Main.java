17e28cc88d6f1f82f16b7063530bd2cf5e267ce5
OiGeN <111658379+OiGeN-DyuK@users.noreply.github.com>
2025-11-16 17:28:21 +0100

    lesson_28

---
+++ src/main/java/lesson_28/Main.java
@@ -0,0 +1,14 @@
+package lesson_28;
+
+public class Main {
+    static void main() {
+
+        Shape circle = new Circle(5);
+        circle.displayInfo();
+
+        System.out.println("------------------");
+
+        Shape rectangle = new Rectangle(4, 7);
+        rectangle.displayInfo();
+    }
+}
