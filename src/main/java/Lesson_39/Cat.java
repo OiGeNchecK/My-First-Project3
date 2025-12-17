cc493ed44b763a2b465bb308796d7857fb068692
OiGeN <111658379+OiGeN-DyuK@users.noreply.github.com>
2025-12-17 21:30:43 +0100

    lesson_39

---
+++ src/main/java/Lesson_39/Cat.java
@@ -0,0 +1,15 @@
+package Lesson_39;
+
+public class Cat {
+
+    private String name;
+    private int age;
+
+    public String getName() {
+        return name;
+    }
+
+    public int getAge() {
+        return age;
+    }
+}
---
+++ src/main/java/Lesson_39/Raccoon.java
@@ -0,0 +1,17 @@
+package Lesson_39;
+
+public class Raccoon {
+    private String name;
+    private int age;
+
+    public Raccoon(String name, int age) {
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public int getAge() {
+        return age;
+    }
+}
